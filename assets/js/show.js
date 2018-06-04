518f8ebd57a0aa8f286b0dc636de1fcd747ec8b1
zainal3096 <zainal.makhasin@students.amikom.ac.id>
2018-06-04 15:52:10 +0700

    update 1.0.0

---
+++ assets/js/show.js
@@ -0,0 +1,9 @@
+var app = angular.module('MyApp', [])
+app.controller('MyController', function ($scope) {
+    //This will hide the DIV by default.
+    $scope.IsVisible = false;
+    $scope.ShowHide = function () {
+        //If DIV is visible it will be hidden and vice versa.
+        $scope.IsVisible = $scope.IsVisible ? false : true;
+    }
+});
